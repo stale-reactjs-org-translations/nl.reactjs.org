377ea4366b6f34039917529b6cd24171c2ee08bc
Jacco Kulman <jacco@jaap.nl>
2020-05-23 16:01:05 +0200

    Search translated

--- src/components/LayoutHeader/DocSearch.js
+++ src/components/LayoutHeader/DocSearch.js
@@ -27,7 +27,7 @@ class DocSearch extends Component<{}, State> {
         inputSelector: '#algolia-doc-search',
       });
     } else {
-      console.warn('Search has failed to load and now is being disabled');
+      console.warn('Laden van zoekresultaat is mislukt en wordt nu uitgeschakeld');
       this.setState({enabled: false});
     }
   }
@@ -104,8 +104,8 @@ class DocSearch extends Component<{}, State> {
           }}
           id="algolia-doc-search"
           type="search"
-          placeholder="Search docs"
-          aria-label="Search docs"
+          placeholder="Zoeken"
+          aria-label="Zoeken"
         />
       </form>
     ) : null;
